b3627b15e2e0d2d341713222de7093a850549578
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-07-02 16:25:31 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,4 +3,8 @@ class CreateSudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
+      
+      t.timestamps null: false
+    end
+  end
 end
